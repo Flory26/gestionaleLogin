9f522f9ecbe60e569d9d276c2e47bb6c42032a52
Flory26 <diaconuflory26@gmal.com>
2025-07-15 11:56:31 +0200

    last update

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,50 +1,27 @@
-document
-  .getElementById("loginForm")
-  .addEventListener("submit", function (e) {
-    e.preventDefault();
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
 
-    const username = document.getElementById("usernameLogin").value.trim();
-    const password = document.getElementById("passwordLogin").value.trim();
-    const alertBox = document.getElementById("alertBox");
+  const user = document.getElementById("usernameLogin").value;
+  const password = document.getElementById("passwordLogin").value;
+  console.log(user, password);
 
+  fetch("/assets/db/login.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Impossibile caricare gli utenti");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const utente = data.utenti.find(
+        (u) => u.username === user && u.password === password
+      );
+      console.log(utente);
+      if (utente) {
+        console.log("loggati");
+      } else {
+        console.log("non loggati");
+      }
+    });
+});
 
-    console.log(username, password);
-    
-
-    function showAlert(message, type = "danger") {
-      if (!alertBox) return;
-      alertBox.className = "alert alert-" + type + " mt-3";
-      alertBox.textContent = message;
-      alertBox.classList.remove("invisible");
-      setTimeout(() => {
-        alertBox.classList.add("invisible");
-      }, 5000);
-    }
-
-    fetch("/assets/db/login.json")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Impossibile caricare gli utenti");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        const utente = data.utenti.find(
-          (u) => u.username === username && u.password === password
-        );
-
-        if (utente) {
-          localStorage.setItem("utente", JSON.stringify(utente));
-          showAlert(`Benvenuto, ${utente.username}`, "success");
-
-          setTimeout(() => {
-            window.location.href = "/aggiungiStudente";
-          }, 1000);
-        } else {
-          showAlert("Username o password errati", "danger");
-        }
-      })
-      .catch((err) => {
-        showAlert(err.message, "danger");
-      });
-  });
